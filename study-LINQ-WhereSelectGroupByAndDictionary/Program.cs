84d992bbfe5a5a409c25b8d6d71ea7adf22d9684
Nikita <musatovla3xnikita@gmail.com>
2023-09-13 23:44:26 +0300

    Nikita

---
+++ study-LINQ-WhereSelectGroupByAndDictionary/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_LINQ_WhereSelectGroupByAndDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string>
+            {
+                "Первое",
+                "Второе",
+                "Второе"
+            };
+
+            //=============  WHERE ========== \\
+
+            //without 
+            string str = "";
+            var newlist = new List<string>();
+            foreach (var item in list)
+            {
+                if(item.Contains("П"))
+                    newlist.Add(item);
+            }
+
+            str = string.Join(" ",newlist);
+            Console.WriteLine(str); //Первое
+
+            //with him
+
+            var listTwo = list.Where(c => c.Contains("П"));
+            Console.WriteLine(string.Join("", listTwo));//Первое
+
+
+            //                      Select
+
+            var newlistSelect = list.Select(x => x.Contains("П")); //Do U have 'П'??           
+            Console.WriteLine(string.Join(" ", newlistSelect)); //True False 
+
+            var newlistSelectTwo = list.Select(x => x + 1);
+            Console.WriteLine(string.Join(" ", newlistSelectTwo)); //add number
+
+            //                      OrderBy   sorted* 
+            var newlistOrderBy = list.OrderBy(x => x).ThenBy(x => x.Contains("В"));
+            Console.WriteLine(string.Join (" ", newlistOrderBy)); // Второе Первое
+
+            //                      GroupBy
+
+            var newlistGroupBy = list.GroupBy(q => q).Where(q => q.Count() > 1).Select(g => g.Key);
+            Console.WriteLine(string.Join(" ", newlistGroupBy));    
+        }
+    }
+}
---
+++ study-LINQ-WhereSelectGroupByAndDictionary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("study-LINQ-WhereSelectGroupByAndDictionary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("study-LINQ-WhereSelectGroupByAndDictionary")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2cb7a69d-cd42-4623-96d8-dce855ab3007")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
